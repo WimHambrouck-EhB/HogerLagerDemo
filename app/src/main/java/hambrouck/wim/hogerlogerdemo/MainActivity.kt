4c15d6bc05623d2521b03468990ea4eaafa4dff4
Wim Hambrouck <wim.hambrouck@ehb.be>
2021-03-15 14:31:00 +0100

    invoer opvragen

--- app/src/main/java/hambrouck/wim/hogerlogerdemo/MainActivity.kt
+++ app/src/main/java/hambrouck/wim/hogerlogerdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,16 +15,28 @@ class MainActivity : AppCompatActivity() {
 
         val txtNumber: TextView = findViewById(R.id.txt_number)
         txtNumber.text = "5"
+
+
+        val edtNumber: EditText = findViewById(R.id.editTextNumber)
+
+        // invoer opvragen
+        val tekst = edtNumber.text.toString()
+
+        // tekst converteren naar getal
+        var getal: Int
+        getal = tekst.toInt()
+
+
     }
 
     fun buttonClicked(view: View)
     {
         if (view.id == R.id.btn_lower)
         {
-            Toast.makeText(this, "You clicked lower", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.lower_clicked), Toast.LENGTH_SHORT).show()
 
         } else if (view.id == R.id.btn_equal) {
-            Toast.makeText(this, "You clicked equal", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.equal_clicked), Toast.LENGTH_SHORT).show()
         } else if (view.id == R.id.btn_higher) {
             Log.d("buttonClicked", "Knop higher ingedrukt")
         }
